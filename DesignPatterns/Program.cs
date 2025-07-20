9e57fb153fd6d7814d83c57842a9b0cc9b5789e4
Md. Ashifur Rahaman <ashif.arsb@gmail.com>
2025-07-20 19:07:12 +0600

    Creational | Singleton | Implemented singleton simulation

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatterns.Singleton;
+
+namespace DesignPatterns
+{
+    public class Program
+    {
+        protected Program() { }
+        static void Main(string[] args)
+        {
+            /*****************************************************
+             * Singleton simulation bolck
+             * ***************************************************/
+
+            var singletonSimulation = new SingletonSimulation();
+            singletonSimulation.SimulateDoubleChedkedLockingObject();
+            singletonSimulation.SimulateEgarInitialization();
+            singletonSimulation.SimulateLazyInitialization();
+
+
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> lazyInstance = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get { return lazyInstance.Value; }
+        }
+
+        private Logger()
+        {
+            Console.WriteLine("Logger instance created.");
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] Log: {message}");
+        }
+    }
+}
